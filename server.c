e9c5965fc117202a21248711b9a0b90dbc0795aa
Johnny Koch <johnnykoch02@gmail.com>
2023-08-13 16:53:29 -0400

    DISABLE HTTPS

--- server.c
+++ server.c
@@ -175,7 +175,7 @@ int main(int argc, char** args) {
 //   mg_tls_ctx_init(&mgr, &opts);
   mg_mgr_init(&mgr);  
   mg_http_listen(&mgr, s_http_addr, handle_routes, NULL);  // Create HTTP listener
-  mg_http_listen(&mgr, s_https_addr, handle_routes, (void *) 1);  // HTTPS listener
+//   mg_http_listen(&mgr, s_https_addr, handle_routes, (void *) 1);  // HTTPS listener
   for (;;) mg_mgr_poll(&mgr, 1000); 
   mg_mgr_free(&mgr);
   return 0;
